3300c888fdfad61c9f8b47d9a63b6731dbb53ff8
JunYoung Kim <sk990420@naver.com>
2023-10-05 14:05:02 +0900

    Initial Commit

---
+++ src/main/java/com/junyounggoat/dreamstore/userbatch/UserbatchApplication.java
@@ -0,0 +1,13 @@
+package com.junyounggoat.dreamstore.userbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserbatchApplication.class, args);
+	}
+
+}
